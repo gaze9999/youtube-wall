318833e39ee043656d582a683ffbf5b49d74d952
gaze9999 <gaze9999@livemail.tw>
2020-04-04 00:44:35 +0800

    new layout

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,26 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link>
-    </div>
+    <topbar/>
+    <inputbar/>
     <router-view/>
+    <toastr ref="toastr"></toastr>
   </div>
 </template>
+
+<script>
+export default {
+  components: {
+    topbar: () => import('@/components/topbar.vue'),
+    inputbar: () => import('@/components/inputbar.vue'),
+    toastr: () => import('vue-toastr'),
+  }  
+}
+</script>
 
 <style lang="sass">
 #app
   font-family: Avenir, Helvetica, Arial, sans-serif
   -webkit-font-smoothing: antialiased
   -moz-osx-font-smoothing: grayscale
-  text-align: center
   color: #2c3e50
-
-#nav 
-  padding: 30px
-  a 
-    font-weight: bold
-    color: #2c3e50
-    &.router-link-exact-active
-      color: #42b983
 </style>
