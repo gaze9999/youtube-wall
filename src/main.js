e27417200cfc74c26cdbde231cdf56b880447082
gaze9999 <gaze9999@livemail.tw>
2020-04-04 00:45:12 +0800

    add toastr config

--- src/main.js
+++ src/main.js
@@ -2,10 +2,19 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import 'bootstrap' // Import js file
-import 'bootstrap/dist/css/bootstrap.min.css' // Import css file
+import 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Toastr from 'vue-toastr';
 
 Vue.config.productionTip = false
+
+const toastrConfig = {
+  defaultTimeout: 2000,
+  defaultProgressBar: false,
+  defaultPosition: "toast-top-right",
+  defaultCloseOnHover: false,
+};
+Vue.use(Toastr, toastrConfig);
 
 new Vue({
   router,
