15c7224bebd9a788ae42c7c41734701ad2df0091
gaze9999 <gaze9999@livemail.tw>
2020-04-04 18:25:09 +0800

    rename

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'youtube-wall'
-    ? '/youtubewall/'
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/youtube-wall/'
     : '/'
 }
