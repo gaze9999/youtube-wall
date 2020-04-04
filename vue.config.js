33a02b365900b63e8136fd65a9a33078a339bb41
gaze9999 <gaze9999@livemail.tw>
2020-04-05 04:00:14 +0800

    adjust deploy method

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,11 @@
 module.exports = {
-  "publicPath": "/",
-  "transpileDependencies": [
-    "vuetify"
+  publicPath: process.env.NODE_ENV === 'production'
+  ? '/youtube-wall/'
+  : '/',
+  outputDir: 'dist', // 產出的資料夾名稱
+  assetsDir: 'dist', // 靜態檔的資料夾名稱
+  filenameHashing: true, // bundle出來的檔案是否要有hash值
+  transpileDependencies: [
+    'vuetify'
   ]
 }
