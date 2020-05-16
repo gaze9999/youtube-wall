b3596a90bd2872c28bbc8294c3651b3ebe952b88
gaze9999 <gaze9999@livemail.tw>
2020-05-16 21:12:11 +0800

    fix empty video list

--- src/views/main.vue
+++ src/views/main.vue
@@ -1,14 +1,14 @@
 <template lang="pug">
 v-container.frames(fluid)
-  v-row
-    videoFrame(v-for='link in videoLinks', :key='link.index', :videoLink='link')
+  v-row(v-if='videoCount > 0')
+    g-video-frame(v-for='link in videoLinks', :key='link.index', :videoLink='link')
 </template>
 
 <script>
 export default {
-  name: 'frame-container',
+  name: 'g-frame-container',
   components: {
-    videoFrame: () => import('@/components/videoFrame.vue')
+    'g-video-frame': () => import('@/components/videoFrame.vue')
   },
   data() {
     return {
@@ -16,12 +16,18 @@ export default {
         index: Number,
         videoId: String
       }],
-      videoCount: this.$store.state.linkCount,
-      linkIndex: Number,
+      videoCount: this.$store.state.videoStore.length,
     }
   },
   created() {
-    this.videoLinks = this.$store.state.videoStore
+    if (this.$store.state.videoStore.length > 0) {
+      this.videoLinks = this.$store.state.videoStore
+    }
+  },
+  updated() {
+    this.videoCount = this.$store.state.videoStore.length
+    this.$log.debug('videoLinks: ', this.videoLinks)
+    this.$log.debug('videoCount: ', this.$store.state.videoStore.length)
   },
 }
 </script>
