329afd7707c11ddb65c4de7318d65a6e763b8694
gaze9999 <gaze9999@livemail.tw>
2020-05-29 22:37:23 +0800

    vtuber-card init

---
+++ src/components/vtuberCard.vue
@@ -0,0 +1,25 @@
+<template lang='pug'>
+div
+  v-card
+  
+</template>
+
+<script>
+export default {
+  name: 'g-vtuber-card',
+  props: {
+  },
+  data() {
+    return {
+    }
+  },
+  created() {
+
+  },
+  methods: {
+  }
+}
+</script>
+
+<style scoped lang='sass'>
+</style>
--- src/views/vtuberList.vue
+++ src/views/vtuberList.vue
@@ -1,9 +1,13 @@
 <template lang="pug">
 div
+  g-vtuber-card
 </template>
 
 <script>
 export default {
+  components: {
+    'g-vtuber-card': () => import('@/components/vtuberCard.vue')
+  }
   
 }
 </script>
