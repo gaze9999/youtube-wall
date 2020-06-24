b635432a53dc0556b9aec7fbb2183ab6aa53c7f3
gaze9999 <gaze9999@livemail.tw>
2020-06-24 20:34:13 +0800

    higher button z-index

--- src/components/videoFrame.vue
+++ src/components/videoFrame.vue
@@ -1,52 +1,52 @@
 <template lang='pug'>
 v-scroll-y-transition
-  v-col.youtube_frame
-    v-hover(v-slot:default='{ hover }', close-delay='400')
-      div
-        v-btn(icon=''
-              @click.stop='remove'
-              color='white'
-              absolute
-              large
-              right
-              :style='`opacity: ${hover ? 1 : .2}`'
-        )
-          v-icon mdi-close-circle
-        v-btn(icon=''
-              v-if='!vLinkLoop'
-              @click.stop='setLoop'
-              color='white'
-              absolute
-              large
-              right
-              :style='`opacity: ${hover ? 1 : 0}; top: 4em;`'
-        )
-          v-icon mdi-sync
-        v-btn(icon=''
-              v-if='vLinkLoop'
-              @click.stop='setLoop'
-              color='white'
-              absolute
-              large
-              right
-              :style='`opacity: ${hover ? 1 : .5}; top: 4em;`'
-        )
-          v-icon mdi-sync
-    youtube(:id='"player-" + vLinkId'
-            :video-id='videoLink.videoId'
-            ref='youtube'
-            width='100%'
-            height='100%'
-            @playing='playing'
-            @ended='ended'
-    )
-    iframe.youtube_chat(:id='"chat-" + vLinkId'
-                        :src='vLinkChat'
-                        v-if='this.$store.state.appbar.controlbarStatus.chat'
-                        type='text/html'
-                        allowfullscreen
-                        dark_theme='1'
-    )
+    v-col.youtube_frame
+      v-hover(v-slot:default='{ hover }', close-delay='400')
+        div.v_btn
+          v-btn(icon=''
+                @click.stop='remove'
+                color='white'
+                absolute
+                large
+                right
+                :style='`opacity: ${hover ? 1 : .2}`'
+          )
+            v-icon mdi-close-circle
+          v-btn(icon=''
+                v-if='!vLinkLoop'
+                @click.stop='setLoop'
+                color='white'
+                absolute
+                large
+                right
+                :style='`opacity: ${hover ? 1 : 0}; top: 4em;`'
+          )
+            v-icon mdi-sync
+          v-btn(icon=''
+                v-if='vLinkLoop'
+                @click.stop='setLoop'
+                color='white'
+                absolute
+                large
+                right
+                :style='`opacity: ${hover ? 1 : .5}; top: 4em;`'
+          )
+            v-icon mdi-sync
+      youtube(:id='"player-" + vLinkId'
+              :video-id='videoLink.videoId'
+              ref='youtube'
+              width='100%'
+              height='100%'
+              @playing='playing'
+              @ended='ended'
+      )
+      iframe.youtube_chat(:id='"chat-" + vLinkId'
+                          :src='vLinkChat'
+                          v-if='this.$store.state.appbar.controlbarStatus.chat'
+                          type='text/html'
+                          allowfullscreen
+                          dark_theme='1'
+      )
 </template>
 
 <script>
@@ -157,4 +157,7 @@ export default {
   max-width: 50%
   flex-basis: 50%
   border: 0px
+.v_btn
+  button
+    z-index: 50
 </style>
